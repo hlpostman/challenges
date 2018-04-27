9251a57071282a7b64fd32935e6ff2a723c6c9db
Hannah Lily Postman <hlpostman@Hannahs-MacBook-Pro.local>
2018-04-26 18:54:33 -0700

    Google python style docstring

--- algorithms/interleave.py
+++ algorithms/interleave.py
@@ -4,8 +4,17 @@
 import pytest
 
 def interleave(stack):
-    # Takes a stack and interleaves the elements in place.
-    # This function has a side effect and no return value.
+    """Takes a stack and interleaves the elements in place.
+
+    Args:
+        stack: A list
+
+    Returns:
+        None
+
+    Raises:
+        TypeError: An error occured due to non-list input.
+    """
     for i in range(1,len(stack)):
         queue = []
         for j in range(len(stack) - i):
