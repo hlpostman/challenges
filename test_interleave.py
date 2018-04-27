85d4a4ce9c973087f52d13bfde96eb78445606b8
Hannah Lily Postman <hlpostman@Hannahs-MacBook-Pro.local>
2018-04-26 17:11:57 -0700

    Adjust tests to reflect changing function from returning to non-returning

--- test_interleave.py
+++ test_interleave.py
@@ -1,5 +1,5 @@
 # test_interleave.py
-# 22 April 2018
+# 26 April 2018
 
 import pytest
 import interleave
@@ -7,21 +7,34 @@ from itertools import chain
 
 # Extreme input
 def test_null_input():
-    assert interleave.interleave([]) == []
+    stack = []
+    interleave.interleave(stack)
+    assert stack == []
 def test_singleton_input():
-    assert interleave.interleave([4]) == [4]
+    stack = [4]
+    interleave.interleave(stack)
+    assert stack == [4]
 def test_no_visible_chanage():
-    assert interleave.interleave([1,2]) == [1,2]
-    assert interleave.interleave([3,3,3,3,3]) == [3,3,3,3,3]
+    stack1 = [1,2]
+    interleave.interleave(stack1)
+    assert stack1 == [1,2]
+    stack2 = [3,3,3,3,3]
+    interleave.interleave(stack2)
+    assert stack2 == [3,3,3,3,3]
 def test_smallest_size_visible_change():
-    assert interleave.interleave([1,2,3]) == [1,3,2]
-    assert interleave.interleave([1,2,3,4]) == [1,4,2,3]
+    stack1 = [1,2,3]
+    interleave.interleave(stack1)
+    assert stack1 == [1,3,2]
+    stack2 = [1,2,3,4]
+    interleave.interleave(stack2)
+    assert stack2 == [1,4,2,3]
 def test_large_input():
     a = [i for i in range(10000)]
     b_1 = [i for i in range(5000)]
     b_2 = [i for i in range(9999, 4999, -1)]
     b = list(chain.from_iterable((b_1[i], b_2[i]) for i in range(5000)))
-    assert interleave.interleave(a) == b
+    interleave.interleave(a)
+    assert a == b
 
 # Bad input
 def test_non_stack_input():
