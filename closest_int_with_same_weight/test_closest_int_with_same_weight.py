07ac6e3d65e985f350b6dfe96048741fc0c7dd0a
Emerson <emerson@Emersons-iMac.local>
2018-06-16 11:57:09 -0700

    Tests for closest_int_with_same_weight

--- closest_int_with_same_weight/test_closest_int_with_same_weight.py
+++ closest_int_with_same_weight/test_closest_int_with_same_weight.py
@@ -1,18 +1,32 @@
 # test_closest_int_with_same_weight.py
-# 13 June 2018
+# 16 June 2018
 
 import pytest
-import closest_int_with_same_weight
+from closest_int_with_same_weight import closest_int_with_same_weight
 
 
 # Test valid input
+
 def test_basic_case_1():
     assert closest_int_with_same_weight(1) == 2
-
-def test_extreme_case_1():
-    assert closest_int_with_same_weight(0) == 0
 
 def test_lsb_neighbor_already_set():
     assert closest_int_with_same_weight(3) == 5
 
+def test_value_error():
+    # No 64-bit integer with the same weight as the integer passed exists for these values.
+    with pytest.raises(ValueError):
+        closest_int_with_same_weight(0)
+        closest_int_with_same_weight(18446744073709552000)
+        closest_int_with_same_weight(18446744073709552001)
+
 # Test bad input
+
+def test_type_error():
+    with pytest.raises(TypeError):
+        closest_int_with_same_weight(1.0)
+        closest_int_with_same_weight(-1)
+        closest_int_with_same_weight([])
+        closest_int_with_same_weight("")
+        closest_int_with_same_weight(1,1)
+        closest_int_with_same_weight()
