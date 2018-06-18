c36edac64bbebc8b7e73d37ec4726350587e8c79
Emerson <emerson@Emersons-iMac.local>
2018-06-17 18:48:07 -0700

    Refactor to accommodate different integer sizes

--- closest_int_with_same_weight/closest_int_with_same_weight.py
+++ closest_int_with_same_weight/closest_int_with_same_weight.py
@@ -1,24 +1,37 @@
 # closest_int_with_same_weight.py
-# 16 June 2018
+# 17 June 2018
 
-def closest_int_with_same_weight(n):
+def closest_int_with_same_weight(n, int_size=64):
     """ Returns the nearest integer to n that has the same weight (number of set bits) as n, where "nearest" means the that the absolute value of the difference of n and the integer returned is as small as possible. Integer returned is guaranteed to be different than n.
 
     Args:
         n: an unsigned 64-bit integer
+        int_size: an optional argument specifying the size of the integer passed.  Default is 64.  Other valid values are 4, 8, 16, and 32.
 
     Returns:
         an unsigned 64-bit integer
 
     Raises:
-        ValueError: n is signed or does not fit in 64 bits
-        TypeError: n is not an integer
+        TypeError: n is not an integer and/or int_size is not an integer
+        ValueError: 1) int_size is not a valid integer size
+                    2) n is larger than the integer size passed
+                    3) n is all 0's or all 1's.  No other integer of the same size with the same weight exists
     """
-    INT_SIZE = 64
-    for i in range(INT_SIZE-1):
+    # Error handling
+    if not isinstance(int_size, int):
+        raise TypeError('Invalid type passed for int_size argument.  The int_size must be an integer.')
+    if int_size not in (4,8,16,32,64):
+        raise ValueError('Invalid integer size.  Valid integer sizes are 4, 8, 16, 32, and 64.')
+    if n > 2**int_size:
+        raise ValueError('Integer passed is too large for integer size.  Integer size is an optional arguement defaulting to 64.  Other valid values are 4, 8, 16, and 32.  Integer passed: %d.  Integer size passed: %d.'%(n, int_size))
+
+    # Algorithm
+    for i in range(int_size-1):
         # Find the lsb that has an unset neighbor
         if (n >> i)&1 != (n >> (i+1))&1:
             # Swap bits
             n^= (1 << i) | (1 << (i+1))
             return n
-    raise ValueError('All 0\'s or 1\'s. No 64-bit integer with the same weight exists.')
+
+    # Error handling
+    raise ValueError('All 0\'s or 1\'s. No %d-bit integer with the same weight exists.'%int_size)
--- closest_int_with_same_weight/test_closest_int_with_same_weight.py
+++ closest_int_with_same_weight/test_closest_int_with_same_weight.py
@@ -16,17 +16,41 @@ def test_lsb_neighbor_already_set():
 def test_value_error():
     # No 64-bit integer with the same weight as the integer passed exists for these values.
     with pytest.raises(ValueError):
-        closest_int_with_same_weight(0) # All bits unset
-        closest_int_with_same_weight(18446744073709551615) # All bits set
+        # Value Error type 1
+        # Integer size passed is invalid (not 4, 8,16, 32, or 64)
+        closest_int_with_same_weight(5,2)
+        closest_int_with_same_weight(5,65)
+
+        # Value Error type 2
+        # Integer is too large for the integer size passed
         closest_int_with_same_weight(18446744073709551616) # Greater than 64
+        closest_int_with_same_weight(32,4)
+
+        # Value Error type 3
+        # No integer with the same weight as the integer passed exists for the integer and bit-size passed (i.e. integer is 0 and has all bits unset, or is max_int for that size and has all bits set).
+        # All bits unset
+        closest_int_with_same_weight(0)
+        closest_int_with_same_weight(0, 8)
+        # All bits set
+        closest_int_with_same_weight(18446744073709551615)
+        closest_int_with_same_weight(65534, 16)
+
+
 
 # Test bad input
 
 def test_type_error():
+
     with pytest.raises(TypeError):
+        # Bad number of arguments
+        closest_int_with_same_weight(1,1,1)
+        closest_int_with_same_weight()
+        # Bad type in first argument
         closest_int_with_same_weight(1.0)
         closest_int_with_same_weight(-1)
         closest_int_with_same_weight([])
         closest_int_with_same_weight("")
-        closest_int_with_same_weight(1,1)
-        closest_int_with_same_weight()
+        # Bad type in second argument
+        closest_int_with_same_weight(1, 64.0)
+        closest_int_with_same_weight(1, "")
+        closest_int_with_same_weight(1, [])
